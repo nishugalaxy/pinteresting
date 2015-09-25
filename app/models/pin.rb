15ebc6d45f114b0a5dfd8301b67571e53004e143
nishant <nishant.sharma6692@hotmail.com>
2015-09-25 14:06:43 +0530

    endpoint edited

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,7 +7,7 @@ class Pin < ActiveRecord::Base
                   :bucket => 'sevaniketan',
                   :s3_credentials => "#{Rails.root}/config/aws.yml",
                   :path => "resources/:id/:style/:basename.:extension"
-	              :s3_host_name => 's3-us-west-2.amazonaws.com'
+	              Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 	validates :image, presence: true
 	validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,5 @@ class User < ActiveRecord::Base
                   :bucket => 'sevaniketan',
                   :s3_credentials => "#{Rails.root}/config/aws.yml",
                   :path => "resources/:id/:style/:basename.:extension"
- :s3_host_name => 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 end
