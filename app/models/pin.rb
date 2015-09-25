413cb1635a2a0f2bd08f391feecb658aa9211961
nishant <nishant.sharma6692@hotmail.com>
2015-09-25 12:53:56 +0530

    pin.rb changed

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-   	:storage => :s3,
-                  :bucket => 'sevaniketan',
+                	:storage => 's3',
+                	:bucket => 'sevaniketan',
                   :s3_credentials => "#{Rails.root}/config/aws.yml",
                   :path => "resources/:id/:style/:basename.:extension"
 	validates :image, presence: true
