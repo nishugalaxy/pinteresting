92506893c7225ce94aa794efbab8458a1c733481
nishant <nishant.sharma6692@hotmail.com>
2015-09-25 12:53:56 +0530

    new production enviroment aws.yml aded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Pinteresting::Application.configure do
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
