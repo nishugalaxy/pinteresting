653c7862a3f975efb7f92b30f772cddf16ccbd0f
nishant <nishant.sharma6692@hotmail.com>
2015-09-25 12:53:56 +0530

    Double quote error at pin.rb in model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,7 +5,7 @@ class Pin < ActiveRecord::Base
    	:storage => :s3,
                   :bucket => 'sevaniketan',
                   :s3_credentials => "#{Rails.root}/config/aws.yml",
-                  :path => "resources/:id/:style/:basename.:extension 
+                  :path => "resources/:id/:style/:basename.:extension"
 	validates :image, presence: true
 	validates :description, presence: true
 end
