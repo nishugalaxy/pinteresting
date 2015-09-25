695865f909af9fa421a7ce6447ce6551ac844f98
nishant <nishant.sharma6692@hotmail.com>
2015-09-25 13:30:57 +0530

    Models file edited

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,10 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-                	:storage => :s3
-                	:bucket => 'sevaniketan',
+	has_attached_file :image,
+                  :styles => { :medium => "460x>", :thumb => "100x100>",:vnice=> "400x" },
+                  :storage => :s3,
+                  :bucket => 'sevaniketan',
                   :s3_credentials => "#{Rails.root}/config/aws.yml",
                   :path => "resources/:id/:style/:basename.:extension"
 	validates :image, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ActiveRecord::Base
   has_many :pins, dependent: :destroy
 
   validates :name, presence: true
+
+  has_attached_file :image,
+                  :styles => { :medium => "460x>", :thumb => "100x100>",:vnice=> "400x" },
+                  :storage => :s3,
+                  :bucket => 'sevaniketan',
+                  :s3_credentials => "#{Rails.root}/config/aws.yml",
+                  :path => "resources/:id/:style/:basename.:extension"
 end
