fd11e92857ade1c5d9c66f527dcab9911facd1f1
nishant <nishant.sharma6692@hotmail.com>
2015-10-08 23:42:47 +0530

    pin.rb thumb changed to 75x75

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "50x50>"}
+	has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "75x75>"}
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	# do_not_validate_attachment_file_type :image
 	validates :description, presence: true
